a44844bd7532109fe7b2cf07ec005f6c52cc1952
cared1216 <cared1216@gmail.com>
2024-10-09 12:34:45 +0200

    Add files via upload

---
+++ estadisticas_adaptador.ps1
@@ -0,0 +1,5 @@
+﻿Get-NetAdapterStatistics -Name "Ethernet"
+#Se utiliza para obtener estadísticas detalladas sobre un adaptador de red específico
+#En éste caso, estamos solicitando información sobre el adaptador de red llamado Ethernet
+#ReceivedBytes: la cantidad total de bytes recibidos por el adaptador
+#ReceivedUnicastPackets: el número total de paquetes unicast (enviados a una dirección específica), que han sido recibidos. 
